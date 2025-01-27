1f502da0ae5675ac7fc1e9380792fc946ee47b53
HORIZON-Donut <146617835+HORIZON-Donut@users.noreply.github.com>
2025-01-28 04:08:13 +0700

    Add some stuff to script and replace example

--- Lab03/checkpoint004/checkpoint004.ino
+++ Lab03/checkpoint004/checkpoint004.ino
@@ -1,31 +1,43 @@
 #include <LiquidCrystal.h>  
 
-LiquidCrystal lcd( 2, 15, 17, 16, 4, 0);    // RS, E, D4, D5, D6, D7 
+LiquidCrystal lcd( 2, 15, 17, 16, 4, 0);                       // RS, E, D4, D5, D6, D7  
 
-char myString[64];  
-byte customChar0[] = {B00011, B11011, B11111, B00001, B01101, B00101, B00101, B00111};//ป  
-byte customChar1[] = {B00000, B00000, B00000, B11101, B10101, B10101, B10111, B00010};//ญ 
-byte customChar2[8] = {B00000, B00000, B00000, B01101, B01001, B00101, B01001, B01111};//ย 
-byte customChar3[] = {B00000, B00001, B00001, B01111, B01001, B01101, B01001, B01001};//ศ 
+byte customChar0[8] = {B00000, B00000, B00000, B00010, B00010, B00010, B00010, B00011};
+byte customChar1[8] = {B00001, B01111, B00000, B01110, B10001, B01001, B10001, B10001};
+byte customChar2[8] = {B00000, B00000, B00000, B11001, B10001, B01001, B10001, B11111};
+byte customChar3[8] = {B00000, B00000, B00000, B01110, B10001, B01100, B00010, B01100};
+byte customChar4[8] = {B00000, B11111, B00000, B01010, B10101, B10001, B10101, B11001};
+byte customChar5[8] = {B11010, B01101, B00010, B11111, B10001, B10101, B11001, B10001};
+byte customChar6[8] = {B00000, B00000, B00000, B01110, B10001, B01001, B10001, B10001};
+byte customChar7[8] = {B00001, B00011, B11100, B00000, B01110, B10001, B10101, B11001};
 
 void setup() { 
   lcd.begin(16, 2); 
-  pinMode(12,INPUT);  pinMode(13,OUTPUT); 
+  pinMode(12,INPUT);  
+  pinMode(13,OUTPUT); 
  
-  lcd.createChar(0, customChar0); //ป 
-  lcd.createChar(1, customChar1); //ญ 
-  lcd.createChar(2, customChar2); //ย 
-  lcd.createChar(3, customChar3); //ศ 
+  lcd.createChar(0, customChar0);
+  lcd.createChar(1, customChar1);
+  lcd.createChar(2, customChar2);
+  lcd.createChar(3, customChar3);
+  lcd.createChar(4, customChar4);
+  lcd.createChar(5, customChar5);
+  lcd.createChar(6, customChar6);
+  lcd.createChar(7, customChar7);
    
   lcd.setCursor(1,0); 
-  lcd.write((uint8_t)0);//ป 
-  lcd.write((uint8_t)1);//ญ 
-  lcd.write((uint8_t)1);//ญ 
-  lcd.write((uint8_t)2);//ย 
-  lcd.write((uint8_t)3);//ศ  
-  lcd.print(" Panyayot"); 
+  lcd.write((uint8_t)0);
+  lcd.write((uint8_t)1);
+  lcd.write((uint8_t)2);
+  lcd.write((uint8_t)3); 
+  lcd.write((uint8_t)4);
+  lcd.write((uint8_t)5);
+  lcd.write((uint8_t)6);
+  lcd.write((uint8_t)7);
+ 
+  lcd.print(" Kiettisak"); 
   lcd.setCursor(2,1); 
-  lcd.print("Computer Eng"); 
+  lcd.print("AI engineer"); 
 } 
  
 void loop() {  } 
