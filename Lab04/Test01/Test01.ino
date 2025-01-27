c2dc765b8b9a0d47bb2aebfb4083eae980b7d1ad
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-01-28 06:10:58 +0700

    Add stuff to test 1

--- Lab04/Test01/Test01.ino
+++ Lab04/Test01/Test01.ino
@@ -1,6 +1,20 @@
+#define ADC0_pin 36
+#define ADC1_pin 39
+
+int adcVal;
+float voltage;
 
 void setup() {
+	Serial.begin(115200);
 }
 
 void loop() {
+	adcVal = analogRead(ADC0_pin);
+	voltage = (adcVal / 4095.00) * 3.3;
+	Serial.print("Value = ");
+	Serial.print(adcVal);
+	Serial.print("		Voltage = ");
+	Serial.println(voltage);
+	Serial.println(" V");
+	delay(1000);
 }
