9ac9e32b3808f63fd024c7c476948023290c6833
HORIZON-Donut <146617835+HORIZON-Donut@users.noreply.github.com>
2025-01-28 04:58:34 +0700

    Prepare checkpoint 5

--- Lab03/checkpoint005/checkpoint005.ino
+++ Lab03/checkpoint005/checkpoint005.ino
@@ -1,6 +1,37 @@
+#include "DHT.h"
+
+#define DHTPIN 21
+#define DHTTYPE DHT11
+//#define DHTTYPE DHT22
+//#define DHTTYPE DHT21
+
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
+	Serial.begin(9600);
+	Serial.println("DHT11, test!");
+	dht.begin();
 }
 
 void loop() {
+	delay(2000);
+	
+	float h = dht.readHumidity();
+	float t = dht.readTemperature();
+	float f = dht.readTemperature(true);
+
+	if (isnan(h) || isnan(t) || isnan(f))
+	{
+		Serial.println("Fail to read from DHT sensor!");
+		return;
+	}
+
+	Serial.print("Humidity: ");
+	Serial.print(h);
+	Serial.print("%\t");
+	Serial.print("Temperature: ");
+	Serial.print(t);
+	Serial.print(" *C ");
+	Serial.print(f);
+	Serial.print(" *F\t");
 }
