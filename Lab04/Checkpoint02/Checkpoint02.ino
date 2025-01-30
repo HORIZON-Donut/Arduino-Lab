a22b14d087a23e123f256ccfd70153852f368ac6
HORIZON-Donut <146617835+HORIZON-Donut@users.noreply.github.com>
2025-01-30 14:13:21 +0700

    Update Checkpoint02.ino

--- Lab04/Checkpoint02/Checkpoint02.ino
+++ Lab04/Checkpoint02/Checkpoint02.ino
@@ -1,71 +1,68 @@
-int soundPin = 21;
-int ledPin = 13;
-unsigned long previous_time = 0;
-
-bool check_clap()
-{
-	static unsigned long current_time = 0;
-	char = sw;
-	sw = digitalRead(soundPin);
-	if(sw)
-	{
-		return false;
-	}
-	else
-	{
-		current_time = millis();
-		if((current_time - previous_time) > 25)
-		{
-			previous_time = millis();
-			return true;
-		}
-		else
-		{
-			previous_time = millis();
-			return false;
-		}
-	}
+int soundPin = 21; //ต่อกับ GPIO21 (หรือขา SDA) 
+int ledPin = 13; //แสดงผลบน LED0 บนบอร์ด ESP32 Starter Kit 
+unsigned long previous_time=0; 
+bool check_clap() 
+{ 
+ static unsigned long current_time; 
+ char sw; 
+ sw = digitalRead(soundPin); 
+ if (sw) 
+ { 
+ return false; 
+ } 
+ else 
+ { 
+ current_time = millis(); 
+ if ((current_time-previous_time)>25) 
+ { 
+ previous_time = millis(); 
+ return true; 
+ } 
+ else 
+ { 
+ previous_time = millis(); 
+ return false; 
+ } 
+ } 
+} 
+int val; 
+int state; 
+int num_clap; 
+void setup() 
+{ 
+ pinMode(ledPin, OUTPUT); 
+ pinMode(soundPin, INPUT); 
+ Serial.begin (9600); 
+ state =0; 
+ num_clap = 0; 
+ Serial.println("num_clap = 0"); 
 }
 
-int val;
-int state;
-int num_clap;
-
-void setup() {
-	pinMode(ledPin, OUTPUT);
-	pinMode(soundPin, INPUT);
-	Serial.begin(9600);
-	state = 0;
-	num_clap = 0;
-	Serial.println("num_clap = 0");
-}
-
-void loop() {
-	val = check_clap;
-	if(val == true)
-	{
-		Serial.print("Clap detected!!! #");
-		Serial.print(num_clap);
-		num_clap++;
-
-		if(state == 2)
-		{
-			state = 0;
-			Serial.println("	LED is turned off");
-		}
-		else
-		{
-			state++;
-		}
-	}
-
-	if(state == 2)
-	{
-		digitalWrite(ledPin, HIGH);
-		Serial.println("	LED is turned on");
-	}
-	else
-	{
-		digitalWrite(ledPin, LOW);
-	}
-}
+void loop() 
+{ 
+ val = check_clap(); 
+ if (val==true) 
+ { 
+ Serial.print("Clap deteected!!! #"); 
+ Serial.print(num_clap); 
+ num_clap++; 
+ if (state >3) 
+ { 
+ state = 0; 
+ Serial.println(" reset"); 
+ } 
+ else 
+ { 
+ state += 1; 
+ Serial.println(" Stack clep: "); 
+ Serial.println(state);
+ } 
+ } 
+ if (state == 2) {
+ digitalWrite(ledPin, HIGH); //turn LED1 on 
+ }
+ else if (state == 4){
+ digitalWrite(ledPin, LOW); //turn LED1 off
+ state = 0;
+ } 
+} 
