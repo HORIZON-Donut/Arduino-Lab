88e3b61e46e34ee0bec0c1f59052b06ce41f7b89
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-02-03 12:23:38 +0700

    Add stuff to checkpoint 2

--- Lab05/checkpoint002/checkpoint002.ino
+++ Lab05/checkpoint002/checkpoint002.ino
@@ -1,6 +1,19 @@
-
-void setup() {
+//#include <tone.h>
+#include <Tone32.h>
+#define BUZZER_PIN 12
+#define BUZZER_CHANNEL 0
+#define silence 500
+void setup()
+{
+ for(int i = 0;i < 10;i++)
+ {
+ tone(BUZZER_PIN, NOTE_C4, 500, BUZZER_CHANNEL);
+ tone(BUZZER_PIN, NOTE_F5, 500, BUZZER_CHANNEL);
+ }
+ delay(5000);
 }
-
-void loop() {
+void loop()
+{
+ tone(BUZZER_PIN, NOTE_C4, 500, BUZZER_CHANNEL);
+ delay(silence);
 }
