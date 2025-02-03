494f3b922f61b19cfa22876f5588e35ba3ff9730
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-02-03 12:35:10 +0700

    Add note andnote duration array to script

--- Lab05/checkpoint002/checkpoint002.ino
+++ Lab05/checkpoint002/checkpoint002.ino
@@ -3,6 +3,31 @@
 #define BUZZER_PIN 12
 #define BUZZER_CHANNEL 0
 #define silence 500
+
+int note = {
+  NOTE_G4, NOTE_A4, NOTE_B4, NOTE_G4,
+  NOTE_F4, NOTE_G4, NOTE_A4, NOTE_F4,
+  NOTE_E4, NOTE_F4, NOTE_G4, NOTE_E4,
+  NOTE_D4, NOTE_C4,
+
+  NOTE_G4, NOTE_A4, NOTE_B4, NOTE_G4,
+  NOTE_F4, NOTE_G4, NOTE_A4, NOTE_F4,
+  NOTE_E4, NOTE_F4, NOTE_G4, NOTE_E4,
+  NOTE_D4, NOTE_C4
+  };
+
+int noteDurations[] = {
+  500, 500, 500, 500,
+  500, 500, 500, 500,
+  500, 500, 500, 500,
+  1000, 1000,
+
+  500, 500, 500, 500,
+  500, 500, 500, 500,
+  500, 500, 500, 500,
+  1000, 1000
+};
+
 void setup()
 {
  for(int i = 0;i < 10;i++)
