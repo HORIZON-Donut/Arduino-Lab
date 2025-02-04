f3c90a1e7f767c728758dba169e807ed5d59dbaf
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-02-04 14:25:58 +0700

    Add key pad stuff to checkpoint script

--- Lab06/checkpoint/checkpoint.ino
+++ Lab06/checkpoint/checkpoint.ino
@@ -1,14 +1,37 @@
 #include <Wire.h>
 #include <LiquidCrystal.h>
+#include <Keypad_I2C.h> 
+#include <Keypad.h> 
 
 #include "RTClib.h" 
 
+#define I2CADDR 0x20 
+
+const byte ROWS = 4; 
+const byte COLS = 4; 
+ 
+char hexaKeys[ROWS][COLS] = { 
+  {'1','2','3','A'}, 
+  {'4','5','6','B'}, 
+  {'7','8','9','C'}, 
+  {'*','0','#','D'} 
+}; 
+byte rowPins[ROWS] = {7, 6, 5, 4}; 
+byte colPins[COLS] = {3, 2, 1, 0}; 
+
 RTC_DS1307 rtc; 
+Keypad_I2C keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS, I2CADDR); 
 char daysOfTheWeek[7][4] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"}; 
 DateTime now;
 
+//Prototype function
+void I2C_bus_scan(void); 
+
 LiquidCrystal lcd(2, 15, 17, 16, 4, 0);
 void setup() {
+  
+  Wire.begin();
+  keypad.begin();
   lcd.begin(16, 2);
   lcd.clear();
 
@@ -30,20 +53,17 @@ void setup() {
     lcd.print("RTC lost power");
     lcd.setCursor(1, 0);
     lcd.print("lets set time!"); 
-   
-  // Comment out below lines once you set the date & time. 
-    // Following line sets the RTC to the date & time this sketch was compiled 
-    // rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); 
-   
-    // Following line sets the RTC with an explicit date & time 
-    // for example to set January 27 2017 at 12:56 you would call: 
-     rtc.adjust(DateTime(2025, 2, 4, 13, 56, 0)); 
+    
+    rtc.adjust(DateTime(2025, 2, 4, 13, 56, 0)); 
   }
 
   delay(1000);
   lcd.clear();
   lcd.setCursor(0, 0);
-  lcd.print("Set up completed");
+  lcd.print("Set up I2C");
+
+  delay(3000);
+  I2C_bus_scan();
 }
 
 void display_date_serial_monitor(void) 
@@ -70,3 +90,40 @@ void loop() {
   display_date_sserial_monitor();
   delay(100);
 }
+
+void I2C_bus_scan(void) 
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("www.9arduino.com");
+  lcd.setCursor(0, 1); 
+  lcd.print("Scanning ..."); 
+  byte count = 0; 
+ 
+  Wire.begin(); 
+  for (byte i = 8; i < 120; i++)  // Loop คนหา Address  
+  { 
+    Wire.beginTransmission (i); 
+    if (Wire.endTransmission () == 0) 
+      {
+        lcd.clear();
+        lcd.setCursor(0, 0); 
+        lcd.print ("Found address: ");
+        lcd.setCursor(1, 1); 
+        lcd.print (i, DEC); 
+        lcd.print (" (0x"); 
+        lcd.print (i, HEX); 
+        lcd.println (")"); 
+        
+        count++; 
+        delay (1);  
+      }  
+  }
+  lcd.clear()
+  lcd.setCursor(2, 0);
+  lcd.print("Done."); 
+  lcd.setCursor(1, 1);
+  lcd.print ("Found "); 
+  lcd.print(count, DEC); 
+  lcd.print(" device(s)."); 
+}   
