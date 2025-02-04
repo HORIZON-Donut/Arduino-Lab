8559cb25d5b9d834d392307d210dcd709313553e
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-02-04 14:01:08 +0700

    Add lcd setup to script

--- Lab06/checkpoint/checkpoint.ino
+++ Lab06/checkpoint/checkpoint.ino
@@ -1,5 +1,14 @@
+#include <Wire.h>
+#include <LiquidCrystal.h>
 
+LiquidCrystal lcd(2, 15, 17, 16, 4, 0);
 void setup() {
+  lcd.begin(16, 2);
+  lcd.clear();
+
+  lcd.setCursir(2, 0);
+  lcd.print("Start up");
+  delay(3000);
 }
 
 void loop() {
