7a40a5e2e6e0ee0a8256114ced52dae86491a422
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-02-05 01:27:59 +0700

    Checking for function

--- Lab05/checkpoint004/checkpoint004.ino
+++ Lab05/checkpoint004/checkpoint004.ino
@@ -17,24 +17,22 @@ void setLed(uint8_t value, uint8_t position)
   shiftOutMod(data_pin, clock_pin, LSBFIRST, CLOCK_TYPE, CLOCK_DELAY_US, value);
   digitalWrite(strobe_pin, HIGH);
 }
-
 void sendCommand(uint8_t value)
 {
-  digitalWrite(strobe, LOW);
-  shiftOutMod(data1, s_clk, LSBFIRST, CLOCK_TYPE, CLOCK_DELAY_US, value);
-  digitalWrite(strobe, HIGH);
+  digitalWrite(strobe_pin, LOW);
+  shiftOutMod(data_pin, clock_pin, LSBFIRST, CLOCK_TYPE, CLOCK_DELAY_US, value);
+  digitalWrite(strobe_pin, HIGH);
 }
-
 void reset()
 {
   sendCommand(0x40); // set auto increment mode
-  digitalWrite(strobe, LOW);
-  shiftOutMod(data1, s_clk, LSBFIRST, CLOCK_TYPE, CLOCK_DELAY_US, 0xc0);   // set starting address to 0
+  digitalWrite(strobe_pin, LOW);
+  shiftOutMod(data_pin, clock_pin, LSBFIRST, CLOCK_TYPE, CLOCK_DELAY_US, 0xc0);   // set starting address to 0
   for(uint8_t i = 0; i < 16; i++)
   {
-    shiftOutMod(data1, s_clk, LSBFIRST, CLOCK_TYPE, CLOCK_DELAY_US, 0x00);
+    shiftOutMod(data_pin, clock_pin, LSBFIRST, CLOCK_TYPE, CLOCK_DELAY_US, 0x00);
   }
-  digitalWrite(strobe, HIGH);
+  digitalWrite(strobe_pin, HIGH);
 }
 
 void setup() {
