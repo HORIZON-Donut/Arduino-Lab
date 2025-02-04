fad8831dcad206736b4520f5a3504cca7a4f23ba
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-02-05 01:24:54 +0700

    Add stuff into checkpoint

--- Lab05/checkpoint004/checkpoint004.ino
+++ Lab05/checkpoint004/checkpoint004.ino
@@ -1,3 +1,41 @@
+#include "wiring_shift_mod.h"
+
+#define CLOCK_TYPE CLOCK_INVERT
+#define CLOCK_DELAY_US 1
+
+const int strobe_pin =  17;
+const int clock_pin  = 16;
+const int data_pin   = 4;
+
+void setLed(uint8_t value, uint8_t position)
+{
+  pinMode(data_pin, OUTPUT);
+
+  sendCommand(0x44);
+  digitalWrite(strobe_pin, LOW);
+  shiftOutMod(data_pin, clock_pin, LSBFIRST, CLOCK_TYPE, CLOCK_DELAY_US, 0xC1 + (position << 1));
+  shiftOutMod(data_pin, clock_pin, LSBFIRST, CLOCK_TYPE, CLOCK_DELAY_US, value);
+  digitalWrite(strobe_pin, HIGH);
+}
+
+void sendCommand(uint8_t value)
+{
+  digitalWrite(strobe, LOW);
+  shiftOutMod(data1, s_clk, LSBFIRST, CLOCK_TYPE, CLOCK_DELAY_US, value);
+  digitalWrite(strobe, HIGH);
+}
+
+void reset()
+{
+  sendCommand(0x40); // set auto increment mode
+  digitalWrite(strobe, LOW);
+  shiftOutMod(data1, s_clk, LSBFIRST, CLOCK_TYPE, CLOCK_DELAY_US, 0xc0);   // set starting address to 0
+  for(uint8_t i = 0; i < 16; i++)
+  {
+    shiftOutMod(data1, s_clk, LSBFIRST, CLOCK_TYPE, CLOCK_DELAY_US, 0x00);
+  }
+  digitalWrite(strobe, HIGH);
+}
 
 void setup() {
 }
