9a511bfab76a126ac3215fdd84ee9d58e065ea2a
HORIZON-Donut <146617835+HORIZON-Donut@users.noreply.github.com>
2025-02-10 17:00:35 +0700

    Update checkpoint002.ino

--- Lab07/checkpoint002/checkpoint002.ino
+++ Lab07/checkpoint002/checkpoint002.ino
@@ -1,74 +1,139 @@
-#define SegA 4 
-#define SegB 0 
-#define SegC 17 
-#define SegD 16 
-#define SegE 23 
-#define SegF 19 
-#define SegG 18 
-#define DP 5 
-#define ActivateD10 14 
-#define ActivateD01 27 
-//------------------------------- 
-char BIN_TO_7SEGMENT2 (char A) 
-{ 
- static char LOOKUPTB[ ] = 
- { 
- 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 
- 0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71 
- }; 
- return LOOKUPTB[A]; 
-} 
-void display7segment(char data) 
-{ 
- (data & 0x01) ? digitalWrite(SegA, HIGH) : digitalWrite(SegA, LOW); 
- (data & 0x02) ? digitalWrite(SegB, HIGH) : digitalWrite(SegB, LOW); 
- (data & 0x04) ? digitalWrite(SegC, HIGH) : digitalWrite(SegC, LOW); 
- (data & 0x08) ? digitalWrite(SegD, HIGH) : digitalWrite(SegD, LOW); 
- (data & 0x10) ? digitalWrite(SegE, HIGH) : digitalWrite(SegE, LOW); 
- (data & 0x20) ? digitalWrite(SegF, HIGH) : digitalWrite(SegF, LOW); 
- (data & 0x40) ? digitalWrite(SegG, HIGH) : digitalWrite(SegG, LOW); 
-} 
-#include "driver/rtc_io.h" 
-#define USE_EXT0_WAKEUP 1 // 1 = EXT0 wakeup, 0 = EXT1 wakeup 
-#define WAKEUP_GPIO GPIO_NUM_34 
-RTC_DATA_ATTR int bootCount = 0; 
-void setup() { 
- pinMode(SegA, OUTPUT); 
- pinMode(SegB , OUTPUT); 
- pinMode(SegC , OUTPUT); 
- pinMode(SegD , OUTPUT); 
- pinMode(SegE , OUTPUT); 
- pinMode(SegF , OUTPUT); 
- pinMode(SegG , OUTPUT); 
- pinMode(DP, OUTPUT); 
- pinMode(ActivateD10, OUTPUT); 
- pinMode(ActivateD01, OUTPUT); 
- pinMode(WAKEUP_GPIO, INPUT); 
- Serial.begin(115200); 
- delay(1000); 
- ++bootCount; 
- Serial.println("Boot number: " + String(bootCount)); 
- delay(3000); 
- esp_sleep_enable_ext0_wakeup(WAKEUP_GPIO, 1);//wake up using high trigger 
- digitalWrite(ActivateD01, HIGH); 
- digitalWrite(ActivateD10, LOW); 
-} 
-void loop() { 
- int x, tmp; 
- x = digitalRead(GPIO_NUM_34); 
- if (x == 0) 
- { 
- Serial.println("Going to sleep now"); 
- delay(300); 
- esp_deep_sleep_start(); 
- } 
- else 
- { 
- tmp = BIN_TO_7SEGMENT2(1); 
- display7segment(tmp); 
- Serial.println("The CPU is running"); 
- delay(1000); 
- } 
-} 
-//---E:\001_teaching\241-102 Micro and Digital for 
-//AI\projects_ESP32\goto_deep_sleep_wakup_with_SW3_1\ 
+#include "wiring_shift_mod.h"
+#include "driver/rtc_io.h"
+
+#define CLOCK_TYPE CLOCK_INVERT
+#define CLOCK_DELAY_US 1
+#define WAKEUP_GPIO GPIO_NUM_34
+
+const int strobe_pin = 17;
+const int clock_pin = 16;
+const int data_pin = 4;
+
+// Variables for state tracking
+bool isRunning = false;
+unsigned long counter = 0;
+RTC_DATA_ATTR int bootCount = 0;
+
+// Function to convert BCD to 7-segment display format
+char BCD_TO_7SEGMENT(uint8_t A) {
+    static char LOOKUPTB[] = {
+        0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07,
+        0x7F, 0x6F, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71
+    };
+    return LOOKUPTB[A];
+}
+
+// Send command to TM1638
+void sendCommand(uint8_t value) {
+    digitalWrite(strobe_pin, LOW);
+    shiftOutMod(data_pin, clock_pin, LSBFIRST, CLOCK_TYPE, CLOCK_DELAY_US, value);
+    digitalWrite(strobe_pin, HIGH);
+}
+
+// Reset display
+void reset() {
+    sendCommand(0x40);
+    digitalWrite(strobe_pin, LOW);
+    shiftOutMod(data_pin, clock_pin, LSBFIRST, CLOCK_TYPE, CLOCK_DELAY_US, 0xc0);
+    for(uint8_t i = 0; i < 16; i++) {
+        shiftOutMod(data_pin, clock_pin, LSBFIRST, CLOCK_TYPE, CLOCK_DELAY_US, 0x00);
+    }
+    digitalWrite(strobe_pin, HIGH);
+}
+
+// Display single digit
+void displayDigit(char position, uint8_t digit) {
+    char data = BCD_TO_7SEGMENT(digit);
+    char address = 0xc0 | (position << 1);
+    
+    digitalWrite(strobe_pin, LOW);
+    shiftOutMod(data_pin, clock_pin, LSBFIRST, CLOCK_TYPE, CLOCK_DELAY_US, address);
+    shiftOutMod(data_pin, clock_pin, LSBFIRST, CLOCK_TYPE, CLOCK_DELAY_US, data);
+    digitalWrite(strobe_pin, HIGH);
+}
+
+// Display full number
+void displayNumber(unsigned long number) {
+    for(int i = 0; i < 8; i++) {
+        displayDigit(7 - i, number % 10);
+        number /= 10;
+    }
+}
+
+// Read button states
+uint8_t readButtons() {
+    uint8_t buttons = 0;
+    digitalWrite(strobe_pin, LOW);
+    shiftOutMod(data_pin, clock_pin, LSBFIRST, CLOCK_TYPE, CLOCK_DELAY_US, 0x42);
+    
+    pinMode(data_pin, INPUT);
+    for(uint8_t i = 0; i < 4; i++) {
+        uint8_t v = shiftInMod(data_pin, clock_pin, LSBFIRST, CLOCK_TYPE, CLOCK_DELAY_US) << i;
+        buttons |= v;
+    }
+    
+    pinMode(data_pin, OUTPUT);
+    digitalWrite(strobe_pin, HIGH);
+    return buttons;
+}
+
+void setup() {
+    // Initialize pins
+    pinMode(strobe_pin, OUTPUT);
+    pinMode(clock_pin, OUTPUT);
+    pinMode(data_pin, OUTPUT);
+    pinMode(WAKEUP_GPIO, INPUT);
+    
+    Serial.begin(115200);
+    
+    // Configure sleep wakeup
+    esp_sleep_enable_ext0_wakeup(WAKEUP_GPIO, 1);
+    
+    // Initialize display
+    sendCommand(0x8f);  // Set maximum brightness
+    reset();
+    displayNumber(0);   // Show initial 00000000
+    
+    ++bootCount;
+    Serial.println("Boot number: " + String(bootCount));
+}
+
+void loop() {
+    // Check SW3-1 state
+    int switchState = digitalRead(WAKEUP_GPIO);
+    
+    if (switchState == LOW) {
+        // Turn off display and enter sleep mode
+        reset();
+        Serial.println("Entering sleep mode");
+        delay(100);
+        esp_deep_sleep_start();
+    } else {
+        // Normal operation mode
+        uint8_t buttons = readButtons();
+        
+        // Check S8 (Start/Stop)
+        if (buttons & 0x80) {
+            isRunning = !isRunning;
+            delay(200); // Debounce
+        }
+        
+        // Check S1 (Reset)
+        if (buttons & 0x01) {
+            counter = 0;
+            isRunning = false;
+            displayNumber(counter);
+            delay(200); // Debounce
+        }
+        
+        // Update counter if running
+        if (isRunning) {
+            counter++;
+            if (counter > 99999999) {
+                counter = 0;
+            }
+            displayNumber(counter);
+            delay(1); // 1ms delay for counting speed
+        }
+    }
+}
