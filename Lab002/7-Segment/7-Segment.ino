45db7c25149d35e99fd7da011aeea045ae4e719f
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-02-25 05:45:42 +0700

    Made board display number correctly

--- Lab002/7-Segment/7-Segment.ino
+++ Lab002/7-Segment/7-Segment.ino
@@ -1,22 +1,33 @@
 const int DG[2]={27,14};
 const int segMentPin[7]={4,0,17,16,23,19,18};
-const int digit01[7] = (1, 1, 1, 1, 1, 1, 1};
+const int digit01[7] = {1, 1, 1, 1, 1, 1, 1};
 const int digit10[7] = {1, 1, 1, 1, 1, 1, 0};
 const int dot=20;
 int i;
 int digit=0;   
 hw_timer_t * timer = NULL;  /* create a hardware timer */
 
+void Display()
+{
+	const int* disp = (digit == 1) ? digit01:digit10;
+	for(i = 0; i < 7; i++)
+	{
+		digitalWrite(segMentPin[i], (disp[i] == 1)? HIGH:LOW);
+	}
+}
+
 void IRAM_ATTR onTimer(){
   if(digit==0){
     digitalWrite(DG[0],LOW);
     digitalWrite(DG[1],HIGH);
+	Display();
     digit=1;
   }
   else
   {
     digitalWrite(DG[1],LOW);
     digitalWrite(DG[0],HIGH);
+	Display();
     digit=0;    
   }
 }
