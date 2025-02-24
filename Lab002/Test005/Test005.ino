2ad243da0dfe3cba74c76b1e423fccab4d5a44b8
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-02-25 06:18:44 +0700

    Put script into test script

--- Lab002/Test005/Test005.ino
+++ Lab002/Test005/Test005.ino
@@ -1,6 +1,25 @@
+#include <Arduino.h>
 
-void setup() {
+struct Button {
+    const uint8_t PIN;
+    uint32_t numberKeyPresses;
+    bool pressed;
+};
+
+Button button = {34, 0, false};
+
+void ARDUINO_ISR_ATTR isr() {
+    button.numberKeyPresses += 1;
+    button.pressed = true;
 }
-
+void setup() {
+    Serial.begin(115200);
+    pinMode(button.PIN, INPUT_PULLUP);
+    attachInterrupt(button.PIN, isr, FALLING);
+}
 void loop() {
+    if (button.pressed) {
+        Serial.printf("Button  has been pressed %u times\n", button.numberKeyPresses);
+        button.pressed = false;
+    }  
 }
