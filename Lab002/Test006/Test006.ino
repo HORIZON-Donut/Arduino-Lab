929d227e24a20b81c2ec9c431a0d5075fbf76d14
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-02-25 06:49:50 +0700

    Add script into test script

--- Lab002/Test006/Test006.ino
+++ Lab002/Test006/Test006.ino
@@ -1,6 +1,19 @@
+#include <WiFi.h>
+#include "time.h"
+
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 7 * 3600;  // Thailand GMT+7
+const int   daylightOffset_sec = 0;
 
 void setup() {
+  Serial.begin(115200);
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
 }
 
 void loop() {
+  struct tm timeinfo;
+  if (getLocalTime(&timeinfo)) {
+    Serial.println(&timeinfo, "%Y-%m-%d %H:%M:%S");
+  }
+  delay(1000);
 }
