8bbebc5d583a7c9d1f550d8f79e4caf85009c59d
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-02-25 18:20:24 +0700

    Set up code

--- Lab002/RealtimeClock/RealtimeClock.ino
+++ Lab002/RealtimeClock/RealtimeClock.ino
@@ -1,13 +1,28 @@
 #include <WiFi.h>
 #include "time.h"
 
+const char* ssid = "HORIZON-Donut";
+const char* pass = "AllMyFriendsHavePassAway-NeedMySelfToSingThisSong";
+
 const char* ntpServer = "pool.ntp.org";
 const long  gmtOffset_sec = 7 * 3600;  // Thailand GMT+7
 const int   daylightOffset_sec = 0;
 
 void setup() {
   Serial.begin(115200);
+
+  WiFi.begin(ssid, pass);
+  Serial.print("connecting to wifi ");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+	  Serial.print(".");
+	  delay(500);
+  }
+  Serial.print(" Connected!!!");
+
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  Serial.print("Getting time from NTP...");
+  printLocalTime();
 }
 
 void loop() {
