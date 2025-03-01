a29881f3bcede9073a73d8fe23192c24958c0174
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-03-01 22:37:42 +0700

    Add wifi test script into the test script

--- Lab003/Test001/Test001.ino
+++ Lab003/Test001/Test001.ino
@@ -1,6 +1,32 @@
+#include <WiFi.h>
+
+WiFiClient  client;
+const int LED=32;
+const char *ssid = “XXXXX";
+const char *password = “YYYYY";
 
 void setup() {
+  Serial.begin(115200);
+  pinMode(LED,OUTPUT);
+  digitalWrite(LED,HIGH);
+  WiFi.mode(WIFI_STA);  
+  
+  Serial.println("Connecting Wifi...");
+  WiFi.begin(ssid,password);
+  while(WiFi.status() != WL_CONNECTED) {
+    Serial.println("");
+    Serial.println("WiFi not CONNECTED");    
+    delay(1000);
+  }
+  digitalWrite(LED,LOW);    
+  
 }
-
-void loop() {
+void loop()
+{
+  if(WiFi.status() == WL_CONNECTED) {
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());    
+    digitalWrite(LED,!digitalRead(LED));  
+  }
+  delay(1000);  
 }
