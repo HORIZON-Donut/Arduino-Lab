6d25542e9456879d5712a3ad5cb00e194a3fc803
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-03-03 21:31:29 +0700

    Add code to send data to broker

---
+++ Lab003/MQTT-Publisher/MQTT-Publisher.ino
@@ -0,0 +1,47 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// WiFi & MQTT settings
+const char* ssid = "WiFi_SSID";
+const char* password = "WiFi_PASSWORD";
+const char* mqtt_server = "broker.emqx.io";
+const int mqtt_port = 1883;
+const char* mqtt_topic = "esp32/analog";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected!");
+
+  client.setServer(mqtt_server, mqtt_port);
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+    if (client.connect("ESP32Client")) {
+      Serial.println("Connected to MQTT!");
+    } else {
+      Serial.print("Failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" retrying...");
+      delay(2000);
+    }
+  }
+}
+
+void loop() {
+  int analogValue = analogRead(34);  // Read from analog pin 34
+  char msg[10];
+  sprintf(msg, "%d", analogValue);
+  
+  Serial.print("Publishing: ");
+  Serial.println(msg);
+  
+  client.publish(mqtt_topic, msg);
+  delay(2000);  // Publish every 2 seconds
+}
