9ef5a2e9b005690c286a70a8509efeb1bcd3b6b6
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-03-03 22:27:04 +0700

    Add stuff

--- Lab003/MQTT-Publisher/MQTT-Publisher.ino
+++ Lab003/MQTT-Publisher/MQTT-Publisher.ino
@@ -1,75 +1,66 @@
-#include <WiFi.h>
 #include <PubSubClient.h>
 
+// WiFi-Verbindung
 const char* ssid = "HORIZON-Donut";
 const char* password = "20040723";
+WiFiClient wifiClient;
 
-// ThingSpeak MQTT settings
-const char* mqtt_server = "mqtt.thingspeak.com";
-const int mqtt_port = 1883;
-const char* mqtt_user = "NjEeOww2Mwk4Jgk6NSs1ACA";
-const char* mqtt_pass = "0tLxmHBOpGzmDJeiuGh/B0ot";
-const char* mqtt_client_id = "NjEeOww2Mwk4Jgk6NSs1ACA";
-const char* mqtt_topic = "channels/2860416/publish/INOHK3QWRWJW4223";
-const char* subscribe_topic = "channels/2860416/subscribe/MYRPM7AENBOAYJJZ";
-WiFiClient espClient;
-PubSubClient client(espClient);
+// MQTT-Verbindung
+const char* mqtt_server = "mqtt3.thingspeak.com";
+const char* mqtt_username = "NjEeOww2Mwk4Jgk6NSs1ACA";
+const char* mqtt_password = "0tLxmHBOpGzmDJeiuGh/B0ot";
+const char* mqtt_id = "2862145";
+const char* topic = "channels/2862145/publish/INOHK3QWRWJW4223";
+//PubSubClient client(wifiClient);
 
-// Callback function to handle incoming messages
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message received on topic: ");
-  Serial.println(topic);
-  Serial.print("Message: ");
-  
-  // Print the message payload
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-}
+PubSubClient client(mqtt_server, 1883, wifiClient);
+
+// BMP180-Sensor
+Adafruit_BMP085 bmp;
 
 void setup() {
   Serial.begin(115200);
+  delay(10);
   
-  // Connect to WiFi
+  // WLAN verbinden
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
+    delay(500);
     Serial.println("Connecting to WiFi...");
   }
-  Serial.println("WiFi connected!");
+  Serial.println("Connected to WiFi");
 
-  // Connect to the MQTT broker
-  client.setServer(mqtt_server, mqtt_port);
-  client.setCallback(callback);
-  
+  // MQTT-Verbindung
+  client.setServer(mqtt_server, 1883);
   while (!client.connected()) {
-    Serial.println("Connecting to MQTT...");
-    if (client.connect(mqtt_client_id, mqtt_user, mqtt_pass)) {
-      Serial.println("Connected to MQTT!");
-      
-      // Subscribe to the ThingSpeak channel for receiving data
-      client.subscribe(subscribe_topic);
+    Serial.println("Connecting to MQTT Broker...");
+    if (client.connect(mqtt_id, mqtt_username, mqtt_password )) {
+      Serial.println("Connected to MQTT Broker");
     } else {
-      Serial.print("Failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" retrying...");
+      Serial.print("Failed with state ");
+      Serial.println(client.state());
       delay(2000);
     }
+  }
+  
+  // BMP180 initialisieren
+  if (!bmp.begin()) {
+    Serial.println("Could not find a valid BMP180 sensor, check wiring!");
+    while (1) {}
   }
 }
 
 void loop() {
-  // Publish data to ThingSpeak
-  String payload = "hall = " + hallRead();  // data for field1
-  if (client.publish(mqtt_topic, payload.c_str())) {
-    Serial.println("Data published to ThingSpeak: " + payload);
-  } else {
-    Serial.println("Failed to publish data.");
-  }
-  
-  // Call client.loop to keep listening for messages
-  client.loop();
-  
-  delay(15000);  // Publish every 15 seconds
+  // Temperatur auslesen
+  float hall = hallRead();
+
+  // Temperatur über MQTT veröffentlichen
+  String payload = String("field1=") + String(hall);
+  client.publish(topic, payload.c_str());
+mqttPublish 
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" *C");
+
+  delay(10000); // 10 Sekunden warten
 }
